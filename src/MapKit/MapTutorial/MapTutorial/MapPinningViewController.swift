916466f88e9d14281dad9eea857cf4385254074d
Yoonseung Choi <cheh344@gmail.com>
2015-10-23 18:34:51 +0900

    Pinning works
    change image & moving works

---
+++ src/MapKit/MapTutorial/MapTutorial/MapPinningViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MapPinningViewController.swift
+//  MapTutorial
+//
+//  Created by Yoonseung Choi on 2015. 10. 23..
+//  Copyright © 2015년 Yoonseung Choi. All rights reserved.
+//
+
+import UIKit
+
+class MapPinningViewController: UIViewController {
+    override func  viewDidLoad() {
+        OrangePin.imageView!.image = UIImage(named: "GrayPin")
+
+    }
+    
+    var touchedFlag = false
+    
+    @IBOutlet weak var OrangePin: UIButton!
+    
+    @IBAction func touched(sender: AnyObject) {
+        touchedFlag = true
+        OrangePin.imageView!.image = UIImage(named: "OrangePin")!
+        print("on touch")
+    }
+
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if touchedFlag {
+            OrangePin.layer.zPosition = 1
+            OrangePin.center = (touches.first?.locationInView(view))!
+        }
+        print("touch")
+        print(touchedFlag)
+    }
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        touchedFlag = false
+       //OrangePin.imageView?.image = UIImage(named: "GrayPin")
+
+
+        print("off touch")
+    }
+    
+}
